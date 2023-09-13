3b030a452e2199434a6a24969907473ecde1df0b
Artem Pakk <apakk@me.com>
2023-09-13 15:37:17 +0300

    Add comment

--- app/services/feature_flags_service.rb
+++ app/services/feature_flags_service.rb
@@ -1,4 +1,9 @@
 class FeatureFlagsService
+  # Acceptable fallback values:
+  # true | false
+  # 'true' | 'false'
+  # 't' | 'f'
+  # 1 | 0
   def self.is_enabled?(feature_flag_name, fallback_value = false)
     feature_flag = FeatureFlag.find_by(name: feature_flag_name)
 
